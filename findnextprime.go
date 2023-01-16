61217ec65d2228f8c57b0aa70a350fc389aec77d
elndour <ndourffaalloouu@gmail.com>
2023-01-16 18:37:20 +0000

    nbPremieraffich

--- findnextprime.go
+++ findnextprime.go
@@ -2,10 +2,15 @@ package piscine
 
 func FindNextPrime(nb int) int {
 	if nb <= 0 {
-		return 0
+		return 2
 	} else {
 		for i := 2; i < nb; i++ {
 			if nb%i == 0 {
+				for i := 2; i < nb; i++ {
+					if (nb+1)%i == 0 {
+						return nb + 2
+					}
+				}
 				return nb + 1
 			}
 		}
