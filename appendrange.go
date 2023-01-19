de48025ffa2ddf255e1d83af23e2904699da5d99
elndour <ndourffaalloouu@gmail.com>
2023-01-19 18:45:18 +0000

    appendrange

--- appendrange.go
+++ appendrange.go
@@ -1,8 +1,11 @@
 package piscine
 
 func AppendRange(min, max int) []int {
-	var tab []int
+	var tab []int = nil
 	if max < min || max == min || max == 1 {
+		return tab
+	}
+	if max == 0 && min == 0 {
 		return tab
 	}
 	for i := min; i < max; i++ {
--- test/main.go
+++ test/main.go
@@ -1,4 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"piscine"
+)
+
 func main() {
+	fmt.Println(piscine.AppendRange(0, 1))
+	fmt.Println(piscine.AppendRange(10, 5))
 }
